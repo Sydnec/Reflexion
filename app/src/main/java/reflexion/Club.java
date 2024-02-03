510034483949798ec6e4753212400c90b79d68cc
Sydnec <simon.bourlier@etu.mines-ales.fr>
2024-02-03 17:03:14 +0100

    Ajout de la class Club

---
+++ app/src/main/java/reflexion/Club.java
@@ -0,0 +1,50 @@
+package reflexion;
+
+public class Club {
+    private Long id;
+    private int version;
+    private String fabricant;
+    private Double poids;
+
+    // Constructor
+    public Club(Long id, int version, String fabricant, Double poids) {
+        this.id = id;
+        this.version = version;
+        this.fabricant = fabricant;
+        this.poids = poids;
+    }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public String getFabricant() {
+        return fabricant;
+    }
+
+    public Double getPoids() {
+        return poids;
+    }
+    
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public void setFabricant(String fabricant) {
+        this.fabricant = fabricant;
+    }
+
+    public void setPoids(Double poids) {
+        this.poids = poids;
+    }
+}
